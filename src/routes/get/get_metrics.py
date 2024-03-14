cd863535526eaf4f4d333b26869c94a29bba5f52
hernii8 <hernandelafuentelopez@gmail.com>
2024-03-14 14:25:22 +0100

    Add error handling to metrics

--- src/routes/get/get_metrics.py
+++ src/routes/get/get_metrics.py
@@ -3,6 +3,8 @@ from fastapi import Depends, HTTPException
 import jwt
 from server import app, oauth2_scheme, ecg_repo
 from src.application.get.get_metrics import GetMetrics
+from src.domain.ecg.errors.ecg_not_found_error import ECGNotFoundError
+from src.domain.ecg.errors.not_ecg_owner_error import NotECGOwnerError
 from src.infra.shared.jwt import JWTToken
 
 
@@ -14,5 +16,11 @@ def get_metrics(ecg_id: str, token: Annotated[str, Depends(oauth2_scheme)]):
         raise HTTPException(status_code=400, detail="Invalid token")
     if payload.get("is_admin"):
         raise HTTPException(status_code=401, detail="Unauthorized")
-    zero_crossings = GetMetrics(ecg_id=ecg_id, ecg_repo=ecg_repo).execute()
+    try:
+        zero_crossings = GetMetrics(ecg_id=ecg_id, ecg_repo=ecg_repo).execute()
+    except ECGNotFoundError:
+        raise HTTPException(status_code=404, detail="Electrocardiogram not found")
+    except NotECGOwnerError:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     return {"zero_crossings": zero_crossings}
