c4cef39c7c759cebfbf5033d0e0deaed200330dc
hernii8 <hernandelafuentelopez@gmail.com>
2024-03-15 12:31:48 +0100

    Move errors to use cases

--- src/application/get/get_metrics.py
+++ src/application/get/get_metrics.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from src.domain.ecg.ecg_repo import ECGRepository
 from src.domain.ecg.errors.metric_not_calculated_error import MetricNotCalculatedError
 from src.domain.ecg.errors.not_ecg_owner_error import NotECGOwnerError
+from src.domain.user.errors.unauthorized_error import UnauthorizedError
 
 
 @dataclass()
@@ -10,7 +11,9 @@ class GetMetrics:
     ecg_id: str
     user_id: str
 
-    def execute(self) -> int:
+    def execute(self, is_admin: bool) -> int:
+        if is_admin:
+            raise UnauthorizedError
         ecg = self.ecg_repo.get(self.ecg_id)
         if self.user_id != ecg.uploader_id:
             raise NotECGOwnerError
--- src/routes/get/get_metrics.py
+++ src/routes/get/get_metrics.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, HTTPException
 from src.domain.ecg.errors.metric_not_calculated_error import MetricNotCalculatedError
+from src.domain.user.errors.unauthorized_error import UnauthorizedError
 from src.routes.dependencies import ecg_repo
 from src.application.get.get_metrics import GetMetrics
 from src.domain.ecg.errors.ecg_not_found_error import ECGNotFoundError
@@ -14,15 +15,13 @@ router = APIRouter()
 
 @router.get("/ecgs/{ecg_id}/metrics", status_code=200)
 def get_metrics(ecg_id: str, token_content: TokenContent = Depends(validate_token)):
-    if token_content["is_admin"]:
-        raise HTTPException(status_code=401, detail="Unauthorized")
     try:
         zero_crossings = GetMetrics(
             ecg_id=ecg_id, ecg_repo=ecg_repo, user_id=token_content["sub"]
-        ).execute()
+        ).execute(is_admin=token_content["is_admin"])
     except ECGNotFoundError:
         raise HTTPException(status_code=404, detail="Electrocardiogram not found")
-    except NotECGOwnerError:
+    except (NotECGOwnerError, UnauthorizedError):
         raise HTTPException(status_code=401, detail="Unauthorized")
     except MetricNotCalculatedError:
         raise HTTPException(status_code=404, detail="Metric not calculated")
