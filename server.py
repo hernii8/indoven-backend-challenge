7012e9e9274390c2aeb7ebc87efa9a237dbf813d
hernii8 <hernandelafuentelopez@gmail.com>
2024-03-17 18:45:38 +0100

    Poblate database if not testing

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
 import uvicorn
+import os
+from fastapi import FastAPI
 from src.routes.get.get_metrics import router as get_metrics_router
 from src.routes.post.create_ecg import router as create_ecg_router
 from src.routes.post.login import router as login_router
 from src.routes.post.create_user import router as create_user_router
 
+ENV = os.environ.get("ENV", "development")
 app = FastAPI()
 app.include_router(get_metrics_router)
 app.include_router(create_ecg_router)
--- src/routes/dependencies.py
+++ src/routes/dependencies.py
@@ -1,8 +1,34 @@
+import os
+from uuid import uuid4
 from fastapi.security import OAuth2PasswordBearer
 from src.infra.memory_repositories.memory_ecg_repo import MemoryECGRepository
 from src.infra.memory_repositories.memory_user_repo import MemoryUserRepo
+from src.infra.shared.hasher import Hasher
 from src.infra.storage import Storage
 
+ENV = os.environ.get("ENV")
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="jwt")
-user_repo = MemoryUserRepo(Storage())
+if ENV == "test":
+    user_repo = MemoryUserRepo(Storage())
+else:
+    user_repo = MemoryUserRepo(
+        Storage(
+            users=[
+                {
+                    "id": str(uuid4()),
+                    "username": "user",
+                    "password": Hasher.hash("password"),
+                    "roles": [],
+                },
+                {
+                    "id": str(uuid4()),
+                    "username": "admin",
+                    "password": Hasher.hash("password"),
+                    "roles": ["admin"],
+                },
+            ]
+        )
+    )
+
 ecg_repo = MemoryECGRepository(Storage())
