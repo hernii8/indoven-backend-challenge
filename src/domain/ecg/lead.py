3c678ee9e1fd124878d24040dc8775c736744a77
hernii8 <hernandelafuentelopez@gmail.com>
2024-03-19 10:13:06 +0100

    Add default n_samples

--- src/domain/ecg/lead.py
+++ src/domain/ecg/lead.py
@@ -7,3 +7,7 @@ class Lead:
     name: str
     signal: List[int]
     n_samples: int | None = field(default=None)
+
+    def __post_init__(self):
+        if self.n_samples is None:
+            self.n_samples = len(self.signal)
